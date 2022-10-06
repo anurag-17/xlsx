a6242e007b8ad0c0cfa22852f8ae5cd3ef869d53
Anurag Pandey <anurag.pandey17@gmail.com>
2022-10-06 18:46:53 +0530

    remove unneccary

--- purchaseOrders-development/Backend/config.js
+++
@@ -1,8 +0,0 @@
-const AWS = require("aws-sdk");
-
-const config =AWS.config.update({
-    region: "ap-south-1",
-    endpoint: "http://localhost:8000"
-});
-var dynamodb = new AWS.DynamoDB();
-module.exports={config,dynamodb};
